bd8bc32dd8fcf27d9967e8c38aaa5efc8df1f416
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-01-28 19:16:02 +0700

    UI-HEADER VER 1.0

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,49 @@
-import styles from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass,
+    faPlus,
+    faEllipsisVertical,
+} from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
-            
-        </div>
-    </header>
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input className={cx('search-input')} placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+
+                    <div className={cx('splitter')}></div>
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}>
+                    <button className={cx('upload')}>
+                        <FontAwesomeIcon icon={faPlus} />
+                        <font>Upload</font>
+                    </button>
+                    <button className={cx('login')}>
+                        <font>Log in</font>
+                    </button>
+                    <FontAwesomeIcon className={cx('menu')} icon={faEllipsisVertical} />
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
