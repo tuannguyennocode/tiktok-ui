1d438d652c6e9319dde23ed876cce4cb067956ab
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-02-05 21:26:15 +0700

    Add Axios Library

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -3,11 +3,13 @@ import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import HeadlessTippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
+
+import * as searchServices from '~/apiServices/searchServices';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icon';
 import styles from './Search.module.scss';
-
+import { useDebounce } from '~/components/hooks';
 const cx = classNames.bind(styles);
 
 function Search() {
@@ -16,28 +18,25 @@ function Search() {
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
 
+    const debounced = useDebounce(searchValue, 500);
+
     const inputRef = useRef();
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([]);
             return;
         }
         setLoading(true);
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
-            .then((response) => {
-                return response.json();
-            })
-            .then((response) => {
-                setSearchResult(response.data);
-                setLoading(false);
-            })
-            .catch(() => {
-                setLoading(false);
-            });
-
+        const fetchApi =  async () => {
+            const res = await searchServices.search(debounced)
+            setSearchResult(res.data)
+            setLoading(false)
+        }
+        
+        fetchApi()
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [searchValue]);
+    }, [debounced]);
     const handleHideResult = () => {
         setShowResult(false);
         console.log(searchResult);
@@ -72,7 +71,7 @@ function Search() {
                         console.log(showResult);
                     }}
                 />
-                {(!!searchValue && !loading) && (
+                {!!searchValue && !loading && (
                     <button
                         className={cx('clear')}
                         onClick={() => {
